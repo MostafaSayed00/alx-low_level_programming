f2b04edf43219aab1c67b7c81b506f50de693af7
MostafaSayed00 <m.sayed71867@gmail.com>
2023-07-20 19:03:30 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,19 +1,12 @@
 #include "main.h"
 /**
-*print_alphabet_x10 - prints alphabet letters
-* ten times
-*desc: The function uses loop to operate
-*Return: no return
+*mul - function that multipliestwo numbers
+*@a: first number
+*@b: second number
+*Return: returns result
 */
-void print_alphabet_x10(void)
-{
-int counter = 0;
-int letter;
-while (counter++ <= 9)
-{
-for (letter = 'a'; letter <= 'z'; letter++)
-_putchar(letter);
 
-_putchar(10);
-}
+int mul(int a, int b)
+{
+return (a * b);
 }
