eb9303098a5af8f5eae72cd8bdc515a207f79bd4
MostafaSayed00 <m.sayed71867@gmail.com>
2023-07-20 19:04:42 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
-*_islower - controls if a character is in lowercase
-*@c: character to be verified
-*Return: return 0 or 1
+*mul - function that multipliestwo numbers
+*@a: first number
+*@b: second number
+*Return: returns result
 */
-int _islower(int c)
-{
-if (c >= 'a' && c <= 'z')
-return (1);
 
-return (0);
+int mul(int a, int b)
+{
+return (a * b);
 }
