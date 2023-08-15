194d3ba9f5da81488512ffc64ee16c17dc764da1
MostafaSayed00 <m.sayed71867@gmail.com>
2023-08-15 08:18:29 -0700

    asdasd

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*struct dog - a new type of data
+*representinfdgfdgsg a dog
+*@name: name of doggdsfgfdsg
+*@age: age of doggdfsgfgdf
+*@owner: owner's namedfsgdfsgds
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+#endif
+
+
